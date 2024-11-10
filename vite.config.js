febad16cfa312b97a38862b2b9d3f3c16ec2e5ba
Juliet Bhandari <juliet.bhandari@gmail.com>
2024-11-10 21:02:21 +1100

    first commit

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        configure: (proxy, options) => {
+          proxy.on('error', (err, _req, _res) => {
+           console.log('error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+           console.log('Request sent to target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+           console.log('Response received from target:', proxyRes.statusCode, req.url);
+          });
+    },
+
+      },
+    },
+  },
+});
